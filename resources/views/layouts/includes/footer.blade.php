474310f74e3610f225924dc07bf219ed3bf96248
Mamunur Rashid Mamun <mrmamun20162017@gmail.com>
2022-08-16 20:05:28 +0600

    more styles updated

--- resources/views/layouts/includes/footer.blade.php
+++ resources/views/layouts/includes/footer.blade.php
@@ -131,4 +131,9 @@
         </div>
         <!-- Copyright -->
     </footer>
+
+    <!-- Back to top button -->
+    <button type="button" class="btn btn-light btn-floating btn-lg" id="btn-back-to-top">
+        <i class="fas fa-arrow-up"></i>
+    </button>
 </section>
